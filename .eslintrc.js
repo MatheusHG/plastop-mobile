c9ceecf16d0b386bd2385af02ae5b9fe774de377
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-23 14:59:01 -0300

    eslint installed

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    camelcase: 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-//import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { AppLoading } from 'expo';
 
-import Routes from './src/routes'
-
-import { Roboto_300Light, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
-import { OpenSans_600SemiBold, useFonts } from '@expo-google-fonts/open-sans'
+import { Roboto_300Light, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { OpenSans_600SemiBold, useFonts } from '@expo-google-fonts/open-sans';
+import Routes from './src/routes';
 
 export default function Main() {
-
   const [fontsLoaded] = useFonts({
     Roboto_300Light,
     Roboto_400Regular,
     Roboto_500Medium,
-    OpenSans_600SemiBold
-  })
+    OpenSans_600SemiBold,
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
