14e69ed4d60320e517a7780cb8f05654462618c9
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-07-28 02:14:22 -0300

    update title

--- src/routes.tsx
+++ src/routes.tsx
@@ -45,11 +45,11 @@ export default function Navigation() {
         <Stack.Screen name="GerenciamentoHome" component={GerenciamentoHome} options={{ title: 'Produtos' }} />
         <Stack.Screen name="GerenciamentoCadastrar" component={GerenciamentoCadastrar} options={{ title: 'Produto' }} />
         <Stack.Screen name="NewPedidoHome" component={NewPedidoHome} options={{ title: 'Realizar Pedido' }} />
-        <Stack.Screen name="NewPedidoConfirmacao" component={NewPedidoConfirmacao} options={{ title: 'NewPedidoConfirmacao' }} />
-        <Stack.Screen name="NewPedidoDadosEntrega" component={NewPedidoDadosEntrega} options={{ title: 'NewPedidoDadosEntrega' }} />
-        <Stack.Screen name="NewPedidoDadosCliente" component={NewPedidoDadosCliente} options={{ title: 'NewPedidoDadosCliente' }} />
-        <Stack.Screen name="AllPedidosHome" component={AllPedidosHome} options={{ title: 'AllPedidosHome' }} />
-        <Stack.Screen name="AllPedidosDetalhes" component={AllPedidosDetalhes} options={{ title: 'AllPedidosDetalhes' }} />
+        <Stack.Screen name="NewPedidoConfirmacao" component={NewPedidoConfirmacao} options={{ title: 'Confirmação' }} />
+        <Stack.Screen name="NewPedidoDadosEntrega" component={NewPedidoDadosEntrega} options={{ title: 'Dados da Entrega' }} />
+        <Stack.Screen name="NewPedidoDadosCliente" component={NewPedidoDadosCliente} options={{ title: 'Dados do Cliente' }} />
+        <Stack.Screen name="AllPedidosHome" component={AllPedidosHome} options={{ title: 'Vendas' }} />
+        <Stack.Screen name="AllPedidosDetalhes" component={AllPedidosDetalhes} options={{ title: 'Detalhes' }} />
         <Stack.Screen name="ClientesHome" component={ClientesHome} options={{ title: 'Clientes' }} />
         <Stack.Screen name="ClientesDados" component={ClientesDados} options={{ title: 'Dados do Cliente' }} />
         <Stack.Screen name="Confirmacao" component={Confirmacao} options={{ headerShown: false }} />
