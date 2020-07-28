89b852fc9c77b838e6dd958ce6e3c3cc18da8074
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-28 13:26:42 -0300

    initial order card added

--- src/pages/AllPedidos/Home/components/OrderCard.tsx
+++ src/pages/AllPedidos/Home/components/OrderCard.tsx
@@ -13,6 +13,19 @@ function OrderCard({ order }: OrderCardProps) {
         <Info>Emissão: </Info>
         <StrongInfo>05/05/2020</StrongInfo>
       </ContainerInfo>
+
+      <Data>
+        <DataContainer>
+          <DataInfo>Nome do Cliente: </DataInfo>
+          <Info>{order.name}</Info>
+          <DataInfo>Telefone: </DataInfo>
+          <Info>{order.phone}</Info>
+          <DataInfo>Cidade: </DataInfo>
+          <Info>{order.city}</Info>
+          <DataInfo>Forma de Pagamento: </DataInfo>
+          <Info>{order.paymentForm}</Info>
+        </DataContainer>
+      </Data>
     </Container>
   );
 }
@@ -31,5 +44,18 @@ const Container = styled.View`
 const ContainerInfo = styled.View`
   flex-direction: row;
 `;
+
+const Data = styled.View`
+  width: 100%;
+  background-color: #efefef;
+`;
+
+const DataContainer = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const DataInfo = styled.Text``;
 
 export default OrderCard;
