ebb4438237183fbe12a23c5c8d1f6f67e9d5dc3e
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-08-25 14:14:08 -0300

    Loading screen added

---
+++ src/pages/LoadingScreen/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, LoadingIcon } from './styles';
+
+function LoadingScreen() {
+  return (
+    <Container>
+      <LoadingIcon size="large" color="#03071E" />
+    </Container>
+  );
+}
+
+export default LoadingScreen;
