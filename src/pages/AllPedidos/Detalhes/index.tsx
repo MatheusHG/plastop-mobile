fb7c8c28dc4d008d94ed1e6a53dd26bbc25c4a07
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-04 17:49:19 -0300

    Whatsapp and email added

--- src/pages/AllPedidos/Detalhes/index.tsx
+++ src/pages/AllPedidos/Detalhes/index.tsx
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect, useState, useEffect } from 'react';
-import { Alert } from 'react-native';
+import { Alert, View, Linking } from 'react-native';
 import { useRoute, useNavigation, RouteProp } from '@react-navigation/native';
-import { Button, Dialog, Paragraph } from 'react-native-paper';
+import {
+  Button, Dialog, Paragraph, TextInput,
+} from 'react-native-paper';
 import {
   StrongInfo, Info,
 } from '../Home/styles';
@@ -27,10 +29,14 @@ function formatPrice(price: number) {
 
 export default function OrderDetails() {
   const route = useRoute<RouteProp<ParamList, 'AllPedidosDetalhes'>>();
+  const theme = { colors: { primary: '#03071E' } };
   const navigation = useNavigation();
   const { codigoPedido } = route.params;
 
+  const [email, setEmail] = useState('');
+  const [sendEmail, setSendEmail] = useState(false);
   const [visible, setVisible] = useState(false);
+  const [share, setShare] = useState(false);
   const [loading, setLoading] = useState(false);
   const [codigo, setCodigo] = useState<string | undefined>('');
   const [total, setTotal] = useState<string | undefined>('');
@@ -50,7 +56,7 @@ export default function OrderDetails() {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => <ShareButton onPress={() => console.log('compartilhou')} />,
+      headerRight: () => <ShareButton onPress={() => setShare(true)} />,
     });
   }, [navigation]);
 
@@ -63,9 +69,9 @@ export default function OrderDetails() {
         const { pedido, produtos }: { pedido: Order, produtos: Product[] } = response.data;
 
         setLoading(false);
-        setCodigo(String(pedido.codigo));
+        setCodigo(pedido.codigo ? String(pedido.codigo) : '');
         setTotal(pedido.total);
-        setCodigoCliente(String(pedido.codigo_cliente));
+        setCodigoCliente(pedido.codigo_cliente ? String(pedido.codigo_cliente) : '');
         setPagamento(pedido.pagamento);
         setObservacao(pedido.observacao);
         setData(pedido.data);
@@ -74,7 +80,7 @@ export default function OrderDetails() {
         setCidade(pedido.cidade);
         setRua(pedido.rua);
         setUf(pedido.uf);
-        setNumero(String(pedido.numero));
+        setNumero(pedido.numero ? String(pedido.numero) : '');
         setTelefone1(pedido.telefone1);
         setTelefone2(pedido.telefone2);
 
@@ -110,6 +116,58 @@ export default function OrderDetails() {
     }
   };
 
+  const handleWpp = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get(`/comprovante?codigo=${codigoPedido}&telefone1=${telefone1}`);
+
+      setLoading(false);
+      setShare(false);
+      Linking.openURL(response.data);
+    } catch (error) {
+      setLoading(false);
+
+      if (error.response) {
+        if (error.response.data.error) {
+          Alert.alert(error.response.data.error);
+        } else {
+          Alert.alert('Ocorreu um erro inesperado.');
+        }
+      } else {
+        Alert.alert('Ocorreu algum erro na comunicação com o servidor.');
+      }
+    }
+  };
+
+  const handleEmail = async () => {
+    if (!email) {
+      Alert.alert('Atenção', 'Preencha o e-mail!');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await api.get(`/comprovante?codigo=${codigoPedido}&to=${email}`);
+
+      Alert.alert(response.data.message);
+
+      setLoading(false);
+      setSendEmail(false);
+    } catch (error) {
+      setLoading(false);
+
+      if (error.response) {
+        if (error.response.data.error) {
+          Alert.alert(error.response.data.error);
+        } else {
+          Alert.alert('Ocorreu um erro inesperado.');
+        }
+      } else {
+        Alert.alert('Ocorreu algum erro na comunicação com o servidor.');
+      }
+    }
+  };
+
   return (
     <>
       <Container>
@@ -128,12 +186,12 @@ export default function OrderDetails() {
               </DataWrap>
 
               <DataWrap width="100%">
-                <StrongInfo>Telefone1: </StrongInfo>
+                <StrongInfo>Telefone 1: </StrongInfo>
                 <Info>{telefone1}</Info>
               </DataWrap>
 
               <DataWrap width="100%">
-                <StrongInfo>Telefone: </StrongInfo>
+                <StrongInfo>Telefone 2: </StrongInfo>
                 <Info>{telefone2}</Info>
               </DataWrap>
 
@@ -184,13 +242,14 @@ export default function OrderDetails() {
         </DetailsContainer>
         <Space />
       </Container>
+
       <Dialog visible={visible} onDismiss={() => setVisible(false)}>
         <Paragraph style={{
           fontSize: 18,
           padding: 15,
         }}
         >
-          Deseja realmente deletar o produto?
+          Deseja realmente deletar o pedido?
 
         </Paragraph>
         <Dialog.Actions>
@@ -198,6 +257,59 @@ export default function OrderDetails() {
           <Button onPress={onDelete} theme={{ colors: { primary: '#4CAF50' } }}>Confirmar</Button>
         </Dialog.Actions>
       </Dialog>
+
+      <Dialog visible={share} onDismiss={() => setShare(false)}>
+        <Paragraph style={{
+          fontSize: 18,
+          padding: 15,
+        }}
+        >
+          Compartilhar com...
+
+        </Paragraph>
+        <Dialog.Actions>
+          <Button onPress={handleWpp} theme={{ colors: { primary: '#4CAF50' } }}>Whatsapp</Button>
+          <Button
+            onPress={() => {
+              setShare(false);
+              setSendEmail(true);
+            }}
+            theme={{ colors: { primary: '#107ba5' } }}
+          >
+            E-mail
+
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+
+      <Dialog visible={sendEmail} onDismiss={() => setSendEmail(false)}>
+        <View style={{
+          height: 200,
+          width: '100%',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: 20,
+        }}
+        >
+          <TextInput
+            label="E-mail do Cliente"
+            style={{
+              width: '100%',
+              marginBottom: 10,
+              backgroundColor: '#FFF',
+              fontWeight: 'bold',
+            }}
+            theme={theme}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <Dialog.Actions>
+            <Button onPress={handleEmail} mode="contained" theme={{ colors: { primary: '#27a333' } }}>Enviar Comprovante</Button>
+          </Dialog.Actions>
+        </View>
+      </Dialog>
+
       <FabButton onPress={() => setVisible(true)} icon="delete-forever" backgroundColor="#e50000" />
       <LoadingModal isVisible={loading} />
     </>
